e88ddcc16ef89cd12d374ba9fb154d325d0b3ec9
mulveyj <github@joemulvey.com>
2017-07-06 12:33:27 +0100

    basic render

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, {Component} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -6,9 +11,21 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello Northcoders!</h1>
+        <Router>
+          <div>
+            <Link to="/">Home</Link>
+            <Route exact path="/" component={Home}/>
+          </div>
+        </Router>
       </div>
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
 
 export default App;
